33d2fa093ea9e527a23fb1ccc83a7e5a139dc4e2
Erik Uckert <erik.uckert@avl.com>
2023-03-08 20:21:06 +0100

    refactor classes

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ class Game {
     this.canvas = null;
     this.ctx = null;
     this.player = null;
+    this.geometry = null;
     this.isMoving = false;
     this.moveTarget = null;
     this.tapStart = null;
@@ -10,65 +11,31 @@ class Game {
     this.isHolding = false;
     this.touchX = null;
     this.touchY = null;
-    this.startX = 0;
-    this.startY = 0;
-    this.endX = 0;
-    this.endY = 0;
-    this.lineLength = 0;
-    this.speed = 0.01;
   }
 
   start() {
-    /**
-     * Initializes the game and sets up any necessary event listeners.
-     * Gets the canvas element and its context, sets its dimensions to match the viewport.
-     * Creates a new player object at the center of the canvas.
-     * Binds touch event listeners to their respective handler methods.
-     * Requests the first animation frame to start the game loop.
-     */
     this.canvas = document.getElementById("game-canvas");
     this.ctx = this.canvas.getContext("2d");
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.player = new Player(this.canvas.width / 2, this.canvas.height / 2);
-    this.canvas.addEventListener(
-      "touchstart",
-      this.handleTouchStart.bind(this)
-    );
-    this.canvas.addEventListener("touchend", this.handleTouchEnd.bind(this));
-    this.canvas.addEventListener("touchmove", this.handleTouchMove.bind(this));
-    requestAnimationFrame(this.gameLoop.bind(this));
+    this.geometry = new Geometry(this, this.player);
+    this.canvas.addEventListener("touchstart", this.handleTouchStart);
+    this.canvas.addEventListener("touchend", this.handleTouchEnd);
+    this.canvas.addEventListener("touchmove", this.handleTouchMove);
+    requestAnimationFrame(this.gameLoop);
   }
 
-  handleTouchStart(event) {
-    /**
-     * Handles the 'touchstart' event when the user touches the screen.
-     * Prevents the default behavior to avoid unwanted scrolling or zooming.
-     * Records the time the touch event started in milliseconds.
-     * Calculates the touch coordinates relative to the canvas element.
-     *
-     * @param {TouchEvent} event - The touch event object.
-     */
-
+  handleTouchStart = (event) => {
     event.preventDefault();
     this.tapStart = new Date().getTime();
 
     // Get the touch coordinates relative to the canvas
     this.touchX = event.changedTouches[0].pageX - this.canvas.offsetLeft;
     this.touchY = event.changedTouches[0].pageY - this.canvas.offsetTop;
-  }
+  };
 
-  handleTouchEnd(event) {
-    /**
-     * Handles the 'touchend' event when the user stops touching the screen.
-     * Prevents the default behavior to avoid unwanted scrolling or zooming.
-     * Calculates the time elapsed between touch start and end in milliseconds.
-     * If the touch duration is less than 200ms, sets the 'isMoving' flag to true and
-     * records the target coordinates of the touch relative to the canvas element.
-     * Resets the 'isHolding' flag to false.
-     *
-     * @param {TouchEvent} event - The touch event object.
-     */
+  handleTouchEnd = (event) => {
     event.preventDefault();
     const tapEnd = new Date().getTime();
     if (tapEnd - this.tapStart < 200) {
@@ -79,18 +46,9 @@ class Game {
       };
     }
     this.isHolding = false;
-  }
+  };
 
-  handleTouchMove(event) {
-    /**
-     * Handles the 'touchmove' event when the user moves their finger on the screen.
-     * Prevents the default behavior to avoid unwanted scrolling or zooming.
-     * Calculates the time elapsed between touch start and current position in milliseconds.
-     * Calculates the touch coordinates relative to the canvas element.
-     * If the touch duration is greater than or equal to 200ms, sets the 'isHolding' flag to true.
-     *
-     * @param {TouchEvent} event - The touch event object.
-     */
+  handleTouchMove = (event) => {
     event.preventDefault();
     const tapEnd = new Date().getTime();
     // Get the touch coordinates relative to the canvas
@@ -99,7 +57,7 @@ class Game {
     if (tapEnd - this.tapStart >= 200) {
       this.isHolding = true;
     }
-  }
+  };
 
   /**
    * Updates the game state based on user input or other factors.
@@ -133,20 +91,55 @@ class Game {
    */
   updateMovingLine() {
     if (this.isHolding) {
-      this.calculateMovingLineStartAndEndPoints();
-      this.calculateMovingLineSpeed();
+      this.geometry.calculateMovingLineStartAndEndPoints();
+      this.geometry.calculateMovingLineSpeed();
     }
 
-    this.calculateMovingLineEndPoint();
-    this.calculateMovingLineStartPoint();
+    this.geometry.calculateMovingLineEndPoint();
+    this.geometry.calculateMovingLineStartPoint();
   }
 
+  render() {
+    // Define the render method, which renders the game
+    // on the canvas or HTML elements
+    // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "#888";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "#fff";
+    this.ctx.beginPath();
+    this.ctx.arc(this.player.x, this.player.y, 10, 0, 2 * Math.PI);
+    this.ctx.fill();
+
+    // Draw the moving line
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.geometry.startX, this.geometry.startY);
+    this.ctx.lineTo(this.geometry.endX, this.geometry.endY);
+    this.ctx.stroke();
+  }
+
+  gameLoop = () => {
+    this.update();
+    this.render();
+    requestAnimationFrame(this.gameLoop);
+  };
+}
+class Geometry {
+  constructor(game, player) {
+    this.game = game;
+    this.player = player;
+    this.startX = 0;
+    this.startY = 0;
+    this.endX = 0;
+    this.endY = 0;
+    this.lineLength = 0;
+    this.speed = 0.01;
+  }
   /**
    * Calculates the start and end points of the moving line based on touch input and player position.
    */
   calculateMovingLineStartAndEndPoints() {
-    this.startX = this.touchX;
-    this.startY = this.touchY;
+    this.startX = this.game.touchX;
+    this.startY = this.game.touchY;
     this.endX = this.player.x;
     this.endY = this.player.y;
     this.lineLength = Math.sqrt(
@@ -166,7 +159,10 @@ class Game {
    * Calculates the end point of the moving line based on its angle and length.
    */
   calculateMovingLineEndPoint() {
-    var angle = Math.atan2(this.endY - this.startY, this.endX - this.startX);
+    var angle = Math.atan2(
+      this.endY - this.startY,
+      this.endX - this.startX
+    );
     this.endX = this.startX + this.lineLength * Math.cos(angle);
     this.endY = this.startY + this.lineLength * Math.sin(angle);
   }
@@ -175,36 +171,16 @@ class Game {
    * Calculates the start point of the moving line based on its angle and speed.
    */
   calculateMovingLineStartPoint() {
-    var angle = Math.atan2(this.endY - this.startY, this.endX - this.startX);
-    this.startX += this.speed * this.lineLength * Math.cos(angle);
-    this.startY += this.speed * this.lineLength * Math.sin(angle);
-  }
-
-  render() {
-    // Define the render method, which renders the game
-    // on the canvas or HTML elements
-    // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.fillStyle = "#888";
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.fillStyle = "#fff";
-    this.ctx.beginPath();
-    this.ctx.arc(this.player.x, this.player.y, 10, 0, 2 * Math.PI);
-    this.ctx.fill();
-
-    // Draw the moving line
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.startX, this.startY);
-    this.ctx.lineTo(this.endX, this.endY);
-    this.ctx.stroke();
-  }
-
-  gameLoop() {
-    this.update();
-    this.render();
-    requestAnimationFrame(this.gameLoop.bind(this));
+    var angle = Math.atan2(
+      this.endY - this.startY,
+      this.endX - this.startX
+    );
+    this.startX +=
+      this.speed * this.lineLength * Math.cos(angle);
+    this.startY +=
+      this.speed * this.lineLength * Math.sin(angle);
   }
 }
-
 // Define the Player class
 class Player {
   constructor(x, y) {
